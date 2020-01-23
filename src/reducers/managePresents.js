a1f293f7f44cbad4c431539cfb133bb83daf7df5
Faris Aziz <farisaziz12@gmail.com>
2020-01-23 11:43:54 +0000

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,21 @@
 export function managePresents(state, action){
 }
+
+const action = {
+    type: "ADD_FRIEND",
+    friend: {
+        name: "Chrome Boi",
+        hometown: "NYC",
+        id: 1
+    }
+}
+
+managePresents = (state, action) => {
+    switch(action.type) {
+        case "INCREASE":
+            return {numberOfPresents: state.numberOfPresents +1}
+        default:
+            return state;
+    }
+}
+
